87ba194692707920fbfcc96d14277e036ab28570
haixiangyan <haixiang6123@gmail.com>
2021-07-18 17:48:11 +0800

    feat: 拆分组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,47 +14,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       home: RandomWords()
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, item) {
-        if(item.isOdd) return Divider();
-
-        final index = item ~/ 2;
-
-        // 添加更多的 word pairs
-        if (index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(generateWordPairs().take(10));
-        }
-
-        // 生成本 row
-        return _buildRow(_randomWordPairs[index]);
-      }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)));
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('WordPair Generator'),
-      ),
-      body: _buildList()
     );
   }
 }
